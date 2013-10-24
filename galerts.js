41eaffbe24608de84eedcfb070da8c0adac881a3
Rob W <rob@winikates.com>
2013-10-24 10:08:00 -0400

    Timestamps added to alert, better handling of sudden spikes, reformatted alert
    Timestamps in alert titles now, alerts now loop to catch up to current
    traffic within one frequency, as opposed to needing several frequencies
    to catch up, and alerts now formatted more attractively.

--- galerts.js
+++ galerts.js
@@ -1,30 +1,33 @@
 if (window.webkitNotifications.checkPermission() == 0) {
   console.log("Notifications are supported!");
   // set initial values
-  var nextAlert = 500;
+  var nextAlert = 500; 
   var lastAlert = nextAlert;
   var incremental = 100;
   
-  var minutes = 5 * 60 * 1000; //(minutes times 60 for seconds times 1000 for milliseconds)
+  var frequency = 5 * 60 * 1000 ; //(minutes times 60 for seconds times 1000 for milliseconds)
   
   var icon = '48.png';
   var mCoinSound = new Audio("https://dl.dropbox.com/u/7079101/coin.mp3");
   
   function getActiveUsers() {
 	 var active = document.getElementById("ID-layout-1374786176014-counterValue").innerHTML;
-	 if (active > nextAlert){
+	 while (active > nextAlert){
 	   lastAlert = nextAlert;
 	   nextAlert = nextAlert + incremental;
+	   var stats = 'Current active users: ' + active + '\nLast alert at: ' + lastAlert+ ' | Next alert at: ' + nextAlert;
 	   
-	   var stats = 'Last alert at: ' + lastAlert + ' active users: ' + active + ' next alert at: ' + nextAlert;
-	   console.log(stats);
+	   var d=new Date();
+	   var n=d.toTimeString();
+	   var timeOfAlert = n.substr(0,5);
+	   var title = 'New traffic alert: ' + timeOfAlert;
 	   
-	   var galert = window.webkitNotifications.createNotification(icon, ' New Traffic Alert', stats);
+	   console.log(timeOfAlert + '\n' +stats);
+	   
+	   var galert = window.webkitNotifications.createNotification(icon, title, stats);
 	   galert.onclick = function(x) { window.focus();}
 	   mCoinSound.play();
 	   galert.show();
-	   
-
 	 }
   }
   setInterval(getActiveUsers, frequency);
