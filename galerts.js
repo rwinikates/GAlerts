a68386158281421f64debd362c4c4b29a5c9eca8
Rob W <rob@winikates.com>
2015-12-31 09:29:22 -0500

    Update to new Notification API

--- galerts.js
+++ galerts.js
@@ -1,33 +1,31 @@
-if (Notification.permission == "granted") {
+if (Notification.permission === "granted") {
   console.log("Notifications are supported!");
   // set initial values
-  var nextAlert = 500; 
+  var nextAlert = 200;
   var lastAlert = nextAlert;
-  var incremental = 100;
-  
+  var incremental = 50;
+
   var frequency = 5 * 60 * 1000 ; //(minutes times 60 for seconds times 1000 for milliseconds)
-  
-  var icon = '48.png';
+
+
   var mCoinSound = new Audio("https://dl.dropbox.com/u/7079101/coin.mp3");
-  
+
   function getActiveUsers() {
-	 var active = document.getElementById("ID-layout-1374786176014-counterValue").innerHTML;
+	 var active = document.getElementById("ID-overviewCounterValue").innerHTML;
 	 while (active > nextAlert){
 	   lastAlert = nextAlert;
 	   nextAlert = nextAlert + incremental;
-	   var stats = 'Current active users: ' + active + '\nLast alert at: ' + lastAlert+ ' | Next alert at: ' + nextAlert;
-	   
+
 	   var d=new Date();
 	   var n=d.toTimeString();
 	   var timeOfAlert = n.substr(0,5);
-	   var title = 'New traffic alert: ' + timeOfAlert;
-	   
+
+	   var stats = 'As of ' + timeOfAlert + ' Current active users: ' + active + '\nLast alert at: ' + lastAlert+ ' | Next alert at: ' + nextAlert;
+
 	   console.log(timeOfAlert + '\n' +stats);
-	   
-	   var galert = window.webkitNotifications.createNotification(icon, title, stats);
-	   galert.onclick = function(x) { window.focus();}
+
+	   var galert = new Notification(stats);
 	   mCoinSound.play();
-	   galert.show();
 	 }
   }
   setInterval(getActiveUsers, frequency);
